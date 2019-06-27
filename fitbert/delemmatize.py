e578375179d14ea623a463d8fb574fad5ecb27ca
Sam Havens <sam.havens@qordoba.com>
2019-06-26 17:44:52 -0700

    add wordnet lemma table

---
+++ fitbert/delemmatize.py
@@ -0,0 +1,10 @@
+"""
+Plan:
+
+Lemmatize word with spacy
+get data/lemma_lookup table, invert
+find all words that share the same lemma
+
+maybe need to open it up a bit by stemming the word,
+then inverse lookup
+"""
