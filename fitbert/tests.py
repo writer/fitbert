433a0fd9843b7e77739115601b83615b51650988
Sam Havens <sam.havens@qordoba.com>
2019-06-27 16:57:49 -0700

    add tests

--- fitbert/tests.py
+++ fitbert/tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from fitbert.delemmatize import Delemmatizer
 
 
@@ -13,8 +15,36 @@ def test_delemmatizer_callable():
 
 
 def test_delemmatizes_lemmas():
-    assert dl("look") == ["looked", "looking", "looks"], "should delemmatize lemmas"
+    assert dl("look") == [
+        "looked",
+        "looking",
+        "looks",
+        "look",
+    ], "should delemmatize lemmas"
 
 
 def test_delemmatizes_non_lemmas():
-    assert dl("running") == ["ran", "running", "runs"], "should delemmatize non-lemmas"
+    assert dl("ran") == [
+        "ran",
+        "running",
+        "runs",
+        "run",
+    ], "should delemmatize non-lemmas"
+
+
+from fitbert import FitBert
+
+
+def test_masker_works_without_instantiating():
+    masked_string, masked = FitBert.mask(
+        "This might be justified to signalling the connection between drunken driving and fatal accidents.",
+        (27, 37),
+    )
+    assert FitBert.mask_token in masked_string, "It should mask using the mask token"
+    assert masked == "signalling", "It should mask the write substring"
+
+
+@pytest.mark.slow
+def test_fitb_initializes():
+    fb = FitBert()
+    assert callable(fb.fitb)
