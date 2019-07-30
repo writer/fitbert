6c3a998862dfd2fb17211799d645fef4793a265a
melisa-qordoba <melisa@qordoba.com>
2019-07-30 12:07:45 +0200

    avoid double masking

--- fitbert/utils.py
+++ fitbert/utils.py
@@ -2,5 +2,5 @@ from typing import Tuple
 
 
 def mask(s: str, span: Tuple[int, int], mask_token="***mask***") -> Tuple[str, str]:
-    subs = s[span[0] : span[1]]
-    return s.replace(subs, mask_token), subs
+    masked_sentence = " ".join([s[: span[0]], mask_token, s[span[1] :]])
+    return masked_sentence, s[span[0] : span[1]]
